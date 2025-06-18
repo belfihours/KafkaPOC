b7ab4346e910d800e809f8f7b22e2702512adafe
belfihours <simorbash@gmail.com>
2025-06-18 20:49:42 +0200

    Initial commit

---
+++ Consumer/Consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:49694",
+    
+    // Fixed properties
+    GroupId = "dev-kafka-consumer",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+const string topic = "purchases";
+
+CancellationTokenSource cts = new CancellationTokenSource();
+Console.CancelKeyPress+= (_, e) =>
+{
+    e.Cancel = true;  // prevent the process from terminating.
+    cts.Cancel();
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+consumer.Subscribe(topic);
+try
+{
+    while (true)
+    {
+        var cr = consumer.Consume(cts.Token);
+        Console.WriteLine($"Consumed event from topic {topic}: key = {cr.Message.Key,-10} value = {cr.Message.Value}");
+    }
+}catch(OperationCanceledException)
+{
+    ;
+}
+finally
+{
+    consumer.Close();
+}
---
+++ Producer/Producer/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using Confluent.Kafka;
+
+const string topic = "purchases";
+
+string[] users = { "simone", "riccardo", "maria", "jbernard", "htanaka", "awalther" };
+string[] items = { "book", "alarm clock", "t-shirts", "gift card", "batteries" };
+
+var config = new ProducerConfig
+{
+    // User-specific properties that you must set
+    BootstrapServers = "localhost:49694",
+    // Fixed properties
+    Acks = Acks.All
+};
+
+using var producer = new ProducerBuilder<string, string>(config).Build();
+
+var numProduced = 0;
+Random rnd = new();
+const int numMessages = 20;
+for (int i = 0; i < numMessages; i++)
+{
+    
+    var user = users[rnd.Next(users.Length)];
+    var item = items[rnd.Next(items.Length)];
+    
+    producer.Produce(topic, new Message<string, string> { Key = user, Value = item },
+        (deliveryPort) =>
+        {
+            if (deliveryPort.Error.Code != ErrorCode.NoError)
+            {
+                Console.WriteLine($"Failed to deliver message: {deliveryPort.Error.Reason}");
+            }
+            else
+            {
+                Console.WriteLine($"Produced event to topic {topic}: key = {user}, value = {item}");
+                numProduced++;
+            }
+        });
+}
+
+producer.Flush(TimeSpan.FromSeconds(10));
+Console.WriteLine($"{numProduced} produced events to topic {topic}.");
